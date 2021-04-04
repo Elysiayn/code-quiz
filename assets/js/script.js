944dc6fd088cb21e38b5ddfb28a380091ece7a25
Elysiayn <wendy.lemus.wl@gmail.com>
2021-04-04 01:28:24 -0400

    finished homepage styling

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+var score = 0;
+
+
+var quizArray = [
+    {
+        question: 'What does HTML stand for?', 
+        a: 'Host Typeface Modem Link',
+        b: 'Hack Terminal Mirror Link',
+        c: 'Hyperlink Terminal Mainframe Logic',
+        d: 'Hypertext Markup Language',
+        answer: 'd'
+    }, 
+    {
+        question: 'What does CSS stand for?', 
+        a: 'Colorful Sheet Styles',
+        b: 'Customer Support Services',
+        c: 'Cascading Style Sheets',
+        d: 'Combat Support Squadron',
+        answer: 'c'
+    },
+    {
+        question: 'What type of extension is a JavaScript file?', 
+        a: '.js',
+        b: '.html',
+        c: '.css',
+        d: '.img',
+        answer: 'a'
+    },
+    {
+        question: 'Which of the following is NOT a programming language?', 
+        a: 'English',
+        b: 'Python',
+        c: 'JavaScript',
+        d: 'Java',
+        answer: 'a'
+    }, 
+    {
+        question: 'Which of the following is used to write comments in JavaScript?', 
+        a: '<!-- <text-here> -->',
+        b: '// <text-here>',
+        c: '/* <text-here> */',
+        d: '# <text-here>',
+        answer: 'b'
+    }, 
+]
+
